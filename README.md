6f3e4122d6923640594e81af8fbbb610aea7d1d0
Alvaro Enrique <31602481+alvaroenrique@users.noreply.github.com>
2018-07-08 20:35:18 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ordenador-de-serie
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,64 @@
+// Constantes
+const miArr = [];
+const input = document.querySelector("input");
+
+const divLista = document.getElementById("lista");
+const divListOrdenada = document.getElementById("listOrdenada");
+const divMensaje = document.getElementById("mensaje");
+
+const btnAgregar = document.getElementById("agregar");
+const btnLimpiar = document.getElementById("limpiar");
+
+//Observadores
+input.addEventListener("keyup", event => {
+  if (event.key == "Enter") {
+    agregarElemento();
+  }
+});
+btnAgregar.addEventListener("click", () => {
+  agregarElemento();
+});
+btnLimpiar.addEventListener("click", () => {
+  limpiar();
+});
+// Funciones
+function agregarElemento() {
+  if (input.value == "" || miArr.includes(parseInt(input.value)) == true) {
+    input.value = "";
+    divMensaje.innerHTML = `<div class="error">
+				<i class="fas fa-exclamation-circle"></i>
+				<span>Tienes que agregar un numero, y que este no se repita en la lista</span>
+			</div>`;
+    input.focus();
+  } else {
+    divMensaje.innerHTML = "";
+    miArr.push(parseInt(input.value));
+    divLista.innerHTML = miArr;
+    input.value = "";
+    divListOrdenada.innerHTML = quickSort(miArr);
+    input.focus();
+  }
+}
+function quickSort(arr) {
+  if (arr.length < 1) {
+    return [];
+  }
+  let left = [];
+  let right = [];
+  let pivot = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [].concat(quickSort(left), pivot, quickSort(right));
+}
+function limpiar() {
+  miArr.length = 0;
+  divLista.innerHTML = "";
+  divListOrdenada.innerHTML = "";
+}
+
+//Compilar para produccion: npm run build
